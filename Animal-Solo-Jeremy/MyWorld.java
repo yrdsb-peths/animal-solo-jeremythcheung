607f52ea0830340457a7a4b6e1871fa857250e2d
jeremythcheung <jeremythcheung@gmail.com>
2026-04-27 12:05:08 -0400

    Day 1 code
    Day 1 code

---
+++ Animal-Solo-Jeremy/MyWorld.java
@@ -0,0 +1,41 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The world our hero lives in.
+ * 
+ * @author (Jeremy) 
+ * @version (April 2026)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant,300,300);
+        
+        //Label scoreLabel = new Label(0,20);
+        //addObject(scoreLabel,0,0);
+        
+        createApple();
+    }
+    
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        
+        int x=Greenfoot.getRandomNumber(600);
+        int y=Greenfoot.getRandomNumber(300);
+        addObject(apple,x,y);
+    }
+}
