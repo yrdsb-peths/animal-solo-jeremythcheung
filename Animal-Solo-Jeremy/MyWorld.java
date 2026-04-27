9b417554402471f408b02c820ff54ce5c9da439c
jeremythcheung <jeremythcheung@gmail.com>
2026-04-27 16:30:41 -0400

    Day 1 complete
    day 1 complete

--- Animal-Solo-Jeremy/MyWorld.java
+++ Animal-Solo-Jeremy/MyWorld.java
@@ -17,7 +17,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
         Elephant elephant = new Elephant();
         addObject(elephant,300,300);
@@ -26,6 +26,11 @@ public class MyWorld extends World
         addObject(scoreLabel,50,50);
         
         createApple();
+    }
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over",100);
+        addObject(gameOverLabel,300,200);
     }
     public void increaseScore()
     {
