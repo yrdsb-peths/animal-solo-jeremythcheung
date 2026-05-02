88c91958c2eced201f5bfbcecff35eeb4fb490bc
jeremythcheung <jeremythcheung@gmail.com>
2026-05-02 09:59:20 -0400

    Auto-save

--- Animal-Solo-Jeremy/Titlescreen.java
+++ Animal-Solo-Jeremy/Titlescreen.java
@@ -17,7 +17,7 @@ public class Titlescreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        GreenfootImage bg = new GreenfootImage("elephant background.jpg");
+        GreenfootImage bg = new GreenfootImage("elephant_background.jpg");
         bg.scale(600, 400);
         setBackground(bg);
 
