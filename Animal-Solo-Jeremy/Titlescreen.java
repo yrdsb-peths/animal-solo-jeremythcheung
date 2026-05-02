25dfb0ab597e579ecbf5a293ade1eb926f658bc6
jeremythcheung <jeremythcheung@gmail.com>
2026-05-02 09:43:42 -0400

    Auto-save

--- Animal-Solo-Jeremy/Titlescreen.java
+++ Animal-Solo-Jeremy/Titlescreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Titlescreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 40);
+    Label titleLabel = new Label("Hungry Elephant", 60);
     /**
      * Constructor for objects of class Titlescreen.
      * 
@@ -18,7 +18,7 @@ public class Titlescreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
 
-        addObject(titleLabel, 150, 50);
+        addObject(titleLabel, 240, 50);
         prepare();
     }
 
